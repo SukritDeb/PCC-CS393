7436e2e6fe46d6b5301c18655922c96430bbeb45
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-10-28 23:09:14 +0530

    Update A1Q4.py

--- A1Q4.py
+++ A1Q4.py
@@ -6,4 +6,4 @@ boxleftover=box%75
 print("No of boxes requied: ",box)
 print("No of container required: ",container)
 print("Left over cookies: ",cookiesleftover)
-print("left over boxes: ",boxleftover)
+print("Left over boxes: ",boxleftover)
