00a4b9b2d477c005e2b82717d3a258e99e7face6
subha03 <roysubhajit2003@gmail.com>
2024-11-17 02:05:31 +0530

    Added A4Q6

---
+++ A4Q6.py
@@ -0,0 +1,10 @@
+l=eval(input("Enter the list: "))
+result=[]
+for i in l:
+    if(l.count(i)==1):
+        result.append(i)
+for i in l:
+    if(l.count(i)>1):
+        result.append(i)
+
+print(result)
