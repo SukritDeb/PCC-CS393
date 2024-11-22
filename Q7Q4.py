aa43df22bf9617b1e6e5e381d80f16222291a929
subha03 <roysubhajit2003@gmail.com>
2024-11-22 16:08:14 +0530

    done A7Q4

--- Q7Q4.py
+++ Q7Q4.py
@@ -1,5 +1,5 @@
 import numpy as np
 matrix1=np.array(eval(input("Enter the matrix 1: ")))
 matrix2=np.array(eval(input("Enter the matrix 2: ")))
-print("The sum of two matrices: ")
-print(matrix1+matrix2)
+print("Element wise multiplication of two matrices: ")
+print(matrix1*matrix2)
