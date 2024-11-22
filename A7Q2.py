b3001ae2e3dbd8f13b7d3db4f7ffa9d831ba06c1
subha03 <roysubhajit2003@gmail.com>
2024-11-22 16:03:08 +0530

    done A7Q2

---
+++ A7Q2.py
@@ -0,0 +1,5 @@
+import numpy as np
+matrix1=np.array(eval(input("Enter the matrix 1: ")))
+matrix2=np.array(eval(input("Enter the matrix 2: ")))
+print("The sum of two matrixes: ")
+print(matrix1+matrix2)
