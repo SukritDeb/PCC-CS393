d99e708622be073277dd66574605139eabc12c59
subha03 <roysubhajit2003@gmail.com>
2024-11-24 11:00:19 +0530

    file handling

---
+++ A9Q3.py
@@ -0,0 +1,22 @@
+f=open("A9Q3_file.txt","r")
+text=f.read()
+space=0
+tab=0
+newline=0
+i=0
+while(i<len(text)):
+    #tab check is painful
+    if(i+4<len(text) and text[i]==' ' and text[i+1]==' ' and text[i+2]==' ' and text[i+3]==' '):
+        tab+=1
+        i=i+4
+    elif(text[i]=='\n'):
+        newline+=1
+        i=i+1
+    elif(text[i]==' '):
+        space+=1
+        i=i+1
+    else:
+        i=i+1
+
+print(f"No of spaces: {space} and No of tabs: {tab} and No of newlines: {newline}")
+f.close()
