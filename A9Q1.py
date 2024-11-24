641c64eaae83bb63fa7fd11fdbac2735d88638f8
subha03 <roysubhajit2003@gmail.com>
2024-11-24 11:21:57 +0530

    A9Q1 done

--- A9Q1.py
+++ A9Q1.py
@@ -1,1 +1,18 @@
-print("hiiii")
+import matplotlib.pyplot as plt
+f=open("A9Q1_file.txt","r")
+text=f.read()
+name=[]
+bill=[]
+text_list=text.split()
+
+for i in range(len(text_list)):
+    if(i%2==0):
+        name.append(text_list[i])
+    else:
+        bill.append(int(text_list[i]))
+
+plt.bar(name,bill)
+plt.title("Monthly Electric Consumption")
+plt.xlabel("Name")
+plt.ylabel("Bill")
+plt.show()
